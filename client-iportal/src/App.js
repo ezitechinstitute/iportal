ab149eae11ea0eb3d65b0b4b2e96173212009aa4
qamar345 <qamargill427@gmail.com>
2024-03-13 14:23:35 +0500

    setup react app

---
+++ client-iportal/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header ">
+        <img src="https://ezitech.org/wp-content/uploads/2023/02/EZITECH-LOGO-white.png"  alt="logo" />
+        <h1> Comming Soon </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
