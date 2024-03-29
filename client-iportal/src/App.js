59178b0adce643da4020bd2fa533dd87750a82eb
qamar345 <qamargill427@gmail.com>
2024-03-29 14:53:24 +0500

    setup manager interface

--- client-iportal/src/App.js
+++ client-iportal/src/App.js
@@ -1,6 +1,7 @@
 import { AdminHome } from "./pages/admin-pages/AdminHome";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { InternHome } from "./pages/interns-pages/InternHome";
+import { ManagerHome } from "./pages/manager-pages/ManagerHome";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="admin-dashboard" element={<AdminHome />} />
+          <Route path="manager-dashboard" element={<ManagerHome />} />
           <Route path="intern-dashboard" element={<InternHome />} />
         </Routes>
       </BrowserRouter>
--- client-iportal/src/pages/interns-pages/InternHome.jsx
+++ client-iportal/src/pages/interns-pages/InternHome.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { InternTopbar } from "../../components/interns-components/InternTopbar";
 import { InternSidebar } from "../../components/interns-components/InternSidebar";
+import { InternDashboard } from "../../components/interns-components/InternDashboard";
 
 export const InternHome = () => {
   return (
     <>
       <InternTopbar />
       <InternSidebar />
+      {/* <InternDashboard /> */}
     </>
   );
 };
