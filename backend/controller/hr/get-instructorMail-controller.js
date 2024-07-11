56c827bd97ae5a08f401127ae5aed3c28d27c351
qamar345 <qamargill427@gmail.com>
2024-07-11 17:23:28 +0500

    update admin dashboard

--- backend/controller/hr/get-instructorMail-controller.js
+++ backend/controller/hr/get-instructorMail-controller.js
@@ -2,7 +2,7 @@ const { connection } = require("../../config/connection");
 
 const GetInstructorEmail = (req, res) => {
   const sql =
-    "SELECT `email` FROM `manager_accounts` WHERE `email` NOT IN ('muzammil@ezitech.org' OR 'kashif@ezitech.org')";
+    "SELECT `email` FROM `manager_accounts` WHERE `email` NOT IN ('muzammil@ezitech.org' AND 'kashif@ezitech.org')";
   connection.query(sql, (err, data) => {
     if (err) throw err;
     return res.json(data);
