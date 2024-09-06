f329b4f65383aea6836070d0483c090d832b346d
qamar345 <qamargill427@gmail.com>
2024-09-06 14:31:45 +0500

    update controllers

--- backend/controller/admin/get-all-interns.js
+++ backend/controller/admin/get-all-interns.js
@@ -2,7 +2,7 @@ const { connection } = require("../../config/connection");
 
 const AdminInterns = (req, res) => {
   const sql =
-    "SELECT * FROM intern_table LEFT JOIN intern_accounts ON intern_table.email = intern_accounts.i_a_email";
+    "SELECT * FROM intern_table LEFT JOIN intern_accounts ON intern_table.email = intern_accounts.email";
 
   db.query(sql, (err, data) => {
     if (err) {
