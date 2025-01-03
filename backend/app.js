491a06e9fdf48afc774b8ea3b8fd1bd14221ec41
qamar345 <qamargill427@gmail.com>
2025-01-03 16:27:35 +0500

    update cors

--- backend/app.js
+++ backend/app.js
@@ -16,9 +16,9 @@ app.use(express.json());
 // Configure CORS options
 const corsOptions = {
   origin: [
-    'https://interns.ezitech.org',
-    'https://manager.ezitech.org',
-    'https://admin.ezitech.org',
+    'https://interns.ezitech.org/',
+    'https://manager.ezitech.org/',
+    'https://admin.ezitech.org/',
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Optional: Allowed methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Optional: Allowed headers
