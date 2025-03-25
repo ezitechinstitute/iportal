0d340861191faf23cfe0567d47bd02f6c2fe7858
qamar345 <qamargill427@gmail.com>
2025-03-25 12:26:19 +0500

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: "35mb" })); // Adjust the limit as needed
 app.use(bodyParser.urlencoded({ limit: "35mb", extended: true }));
 app.use(express.json());
 
-// Configure CORS options
+// Configure CORS options check
 const corsOptions = {
   origin: [
     "https://interns.ezitech.org",
